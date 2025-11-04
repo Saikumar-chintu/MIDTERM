3e4e216839aea027ed43b83da2cd1b6ac50b5e09
Saikumar-chintu <Sk90205n@pace.edu>
2025-11-04 20:35:26 +0530

    Delete CircleApp directory

--- CircleApp/app/src/main/java/com/example/circleapp/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.circleapp.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Purple700 = Color(0xFF5E35B1)
-val Teal700 = Color(0xFF00796B)
-val Orange700 = Color(0xFFF57C00)
-val BgLight = Color(0xFFFFF8E1)
-val TextPrimary = Color(0xFF212121)
